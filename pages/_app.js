845ab242e854df5621e1fc7347fd5a331d768c63
orelsprojects <orelsmail@gmail.com>
2021-07-18 10:15:27 +0300

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import allReducers from '../reducers'
+
+const store = createStore(allReducers, composeWithDevTools())
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 }
 
 export default MyApp
